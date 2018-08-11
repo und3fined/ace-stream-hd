2a1bfc23dd7398aa8680cac4a89eb71988940baa
CuongLe <cuongle.kty@gmail.com>
2018-08-11 20:07:29 +0700

    init script

--- start.sh
+++ start.sh
@@ -5,7 +5,6 @@ echo "- ACE Stream HD for macOS."
 echo "- Made by github.com/und3fined"
 echo "- Original source github.com/und3fined/ace-stream-hd"
 echo ""
-echo "Script starting (Ctrl + C for stop script)."
 echo ""
 
 SOURCE="${BASH_SOURCE[0]}"
@@ -21,4 +20,85 @@ done
 
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
+
+echo "Patch: starting"
+
+cd "$DIR/Contents/Resources/wineprefix/dosdevices"
+ln -sf ../drive_c c:
+ln -sf / z:
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Resources/lib"
+ln -sf libasprintf.0.dylib libasprintf.dylib
+ln -sf libexslt.0.dylib libexslt.dylib
+ln -sf libfreetype.6.dylib libfreetype.dylib
+ln -sf libgettextlib-0.18.2.dylib libgettextlib.dylib
+ln -sf libgettextpo.0.dylib libgettextpo.dylib
+ln -sf libgettextsrc-0.18.2.dylib libgettextsrc.dylib
+ln -sf libgphoto2.2.dylib libgphoto2.dylib
+ln -sf libgphoto2_port.0.dylib libgphoto2_port.dylib
+ln -sf libgsm.1.0.13.dylib libgsm.dylib
+ln -sf libintl.8.dylib libintl.dylib
+ln -sf libjpeg.8.dylib libjpeg.dylib
+ln -sf liblcms2.2.dylib liblcms2.dylib
+ln -sf libltdl.7.dylib libltdl.dylib
+ln -sf libmpg123.0.dylib libmpg123.dylib
+ln -sf libpng16.dylib libpng.dylib
+ln -sf libpng16.la libpng.la
+ln -sf libpng16.16.dylib libpng16.dylib
+ln -sf libsane.1.dylib libsane.dylib
+ln -sf libtalloc.dylib.2 libtalloc.dylib
+ln -sf libtalloc.dylib.2.0.5 libtalloc.dylib.2
+ln -sf libtdb.dylib.1 libtdb.dylib
+ln -sf libtdb.dylib.1.2.9 libtdb.dylib.1
+ln -sf libtiff.5.dylib libtiff.dylib
+ln -sf libtiffxx.5.dylib libtiffxx.dylib
+ln -sf libusb-1.0.0.dylib libusb-1.0.dylib
+ln -sf libusb-0.1.4.dylib libusb.dylib
+ln -sf libwbclient.dylib.0 libwbclient.dylib
+ln -sf libwine.1.0.dylib libwine.1.dylib
+ln -sf libwine.1.dylib libwine.dylib
+ln -sf libxml2.2.dylib libxml2.dylib
+ln -sf libxslt.1.dylib libxslt.dylib
+
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Resources/bin"
+ln -sf winegcc winecpp
+ln -sf winegcc wineg++
+ln -sf libpng16-config libpng-config
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Resources/include"
+ln -sf libpng16/png.h png.h
+ln -sf libpng16/pngconf.h pngconf.h
+ln -sf libpng16/pnglibconf.h pnglibconf.h
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Frameworks/WBottler.framework/Versions"
+ln -sf A Current
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Frameworks/WBottler.framework"
+ln -sf Versions/Current/WBottler WBottler
+ln -sf Versions/Current/Resources Resources
+ln -sf Versions/Current/Headers Headers
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Frameworks/Sparkle.framework/Versions"
+ln -sf A Current
+
+cd "$DIR/Contents/Resources/Wine.bundle/Contents/Frameworks/Sparkle.framework
+ln -sf Versions/Current/Sparkle Sparkle
+ln -sf Versions/Current/Resources Resources
+ln -sf Versions/Current/Headers Headers
+
+cd "$DIR/Contents/Resources/Winetricks.app/Contents/Frameworks/KBActionWindow.framework/Versions"
+ln -sf A Current
+
+cd "$DIR/Contents/Resources/Winetricks.app/Contents/Frameworks/KBActionWindow.framework"
+ln -sf Versions/Current/Resources Resources
+ln -sf Versions/Current/Headers Headers
+ln -sf Versions/Current/KBActionWindow KBActionWindow
+
+echo "Patch: done"
+
+echo "Script starting (Ctrl + C: stop script)."
+echo ""
+
+cd $DIR
 bash "$DIR/Contents/MacOS/startwine"
